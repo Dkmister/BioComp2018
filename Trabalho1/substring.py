7805edcb6230ce79c40dbf51d2765e90533675bf
Vilmar Neto <vilmar.neto@gmail.com>
2018-08-17 02:41:00 -0300

    Retirada de testes
    Arquivo passado em todos testes 💯

--- Trabalho1/substring.py
+++ Trabalho1/substring.py
@@ -4,9 +4,3 @@ def substring_occurence(string,substring):
 		print("Substring not found!\n")
 	else:
 		print("Substring found in index: " + str(occurence_substring) + " until index: " + str(occurence_substring + len(substring)-1) )
-
-word = "cu"
-phrase = "computação é um cu"
-
-substring_occurence(phrase,word)
-substring_occurence(phrase,"y")
