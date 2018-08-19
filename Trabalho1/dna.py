18871d90469086b719f3599e516b0583713d903d
dkmisterv <vilmar.neto@gmail.com>
2018-08-19 18:14:51 -0300

    Funcoes do DNA adicionadas e reescritas

--- Trabalho1/dna.py
+++ Trabalho1/dna.py
@@ -1,4 +1,29 @@
-def change_protein(char):
+def create_nucleotid_UFRGS(number):
+	if number == '0' or number == '7' or number == '8' :
+		return 'A'
+	elif number =='1' or number == '6':
+		return 'T'
+	elif number == '2' or number == '5':
+		return 'G'
+	elif number == '3' or number == '4' or number == '9':
+		return 'C'
+	else:
+		return None
+
+
+def complementary_nucleotid(char):
+	if char == 'T':
+		return 'A'
+	elif char == 'A':
+		return 'T'
+	elif char == 'C':
+		return 'G'
+	elif char == 'G':
+		return 'C'
+	else:
+		return None
+
+def change_all_nucleotids(char):
 	if char == 'T':
 		return ['C','G','A']
 	elif char == 'C':
@@ -7,12 +32,12 @@ def change_protein(char):
 		return ['C','G','T']
 	elif char == 'G':
 		return ['A','C','G']
-		
-def create_mutation(dna):
+
+def create_all_possible_mutations(dna):
 	total_mutations = []
 	others_mutations = []
 	for i in range(0,len(dna)):
-		others_mutations = change_protein(dna[i])
+		others_mutations = change_all_nucleotids(dna[i])
 		for j in range(0,len(others_mutations)):
 			sub_dna = list(dna)
 			(sub_dna[i]) = (others_mutations[j])
