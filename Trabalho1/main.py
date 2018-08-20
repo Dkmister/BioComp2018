f6eba0510ea8fbe72913113b5941b937d0bff7c8
Vilmar Neto <vilmar.neto@gmail.com>
2018-08-20 18:37:47 -0300

    Trabalho 1 finalizado :D

--- Trabalho1/main.py
+++ Trabalho1/main.py
@@ -55,17 +55,45 @@ def substring_palindrome(l, crom):
 			d[lin] = d.get(lin,0) + 1
 	return d
 
-cromossome7 = init_file("sequence.fasta")
+def substring_n(l, crom):
+	d = {}
+	substr = []
+	temp_str =""
+	for line in crom:
+		for i in range(0,len(line) - l):
+			temp_str = line[i:i+l]
+			substr.append(''.join(temp_str))
+	for lin in substr:
+		d[lin] = d.get(lin,0) + 1
+	return d
+
+# Favor Descomentar cada linha, é necessário um computador com pelo menos 4GB de Memória RAM para não acontecer KILL no sistema
+# Sistemas utilizados: Linux Mint e Windows 10
+# Computador com 8GB de Memória RAM, i5 4590K
+#cromossome7 = init_file("sequence.fasta")
+
+
+
 #questao a:
 #locate_diabetes(cromossome7,"CAGGAGATCTTCGTGGCCAC")
+
 #questao b:
 #print(is_palindrome("GC"))
 #print(is_palindrome("ATATG"))
-b = substring_palindrome(8,cromossome7)
-print(b)
+#b = substring_palindrome(8,cromossome7)
+#print("Tamanho de itens com palindromo de tamanho 8: "+str(len(b))+"\n")
+#print(b)
+
+#questao c (GERA UM ARQUIVO *.txt DE 1,5GB!!!):
+#c = substring_n(37,cromossome7)
+#with open("questaoc.txt",'w') as f:
+#	for key, value in c.items():
+#		f.write("Substring: %s :=> Ocorrencia(s): %d\n" % (key,value))
+
 #questao d:
 #qt_nuc = calculate_nucleotids(cromossome7)
 #print(qt_nuc)
+
 #questao e:
 #seq = sequence_nucleotid_UFRGS("00242276")
 #lst_s = create_different_sequences_E(seq)
