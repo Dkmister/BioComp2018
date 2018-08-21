43e2a94e6ee36749ea217c21b8ff7fc2876de2c9
Dkmister <vilmar.neto@hotmail.com>
2018-08-21 15:01:20 -0300

    Adicionado Funcao de inicializacao de matriz

--- Trabalho2/main.py
+++ Trabalho2/main.py
@@ -1,6 +1,14 @@
 from init import *
+
+def init_matrix(n,m)
+	M = []
+	for i in (len(n)+1):
+		for j in (len(m)+1):
+			M[i][j] = 0
+	return M
+
 def main():
 	human = init_file("human.fasta")
 	
 
-main()
+
