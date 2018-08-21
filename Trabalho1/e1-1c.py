15896f0d6feeda2156a725f2bf2df98946659e95
Vilmar Neto <vilmar.neto@gmail.com>
2018-08-20 23:07:46 -0300

    Update e1-1c.py

--- Trabalho1/e1-1c.py
+++ Trabalho1/e1-1c.py
@@ -5,5 +5,5 @@ c7 = init_file("sequence.fasta")
 c = substring_n(37,c7)
 with open("questaoc.txt",'w') as f:
 	for k,v in c.items():
-		f.write("Substring: %s :=> Ocorrencias(s): %d",%(k,v))
+		f.write("Substring: %s :=> Ocorrencias(s): %d" %(k,v))
 	f.close()
