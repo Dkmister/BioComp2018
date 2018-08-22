11800a2bc4a8a68d13077756a89070c9e1ea01a7
Vilmar Neto <vilmar.neto@gmail.com>
2018-08-22 19:10:44 +0000

    Inicializacao da matriz Neddle concluida e valores adicionados

--- Trabalho2/main.py
+++ Trabalho2/main.py
@@ -1,11 +1,33 @@
 from init import *
 
-def init_matrix(n,m)
+match_award = 5
+mismatch_penalty = -3
+gap_penalty = -4
+
+def init_matrix(n,m):
 	M = []
-	for i in (len(n)+1):
-		for j in (len(m)+1):
+	
+	for i in range(0,len(n)+1):
+		for j in range(0,len(m)+1):
 			M[i][j] = 0
+			
+	for i in range(0,len(n)+1):
+		score[i][0] = gap_penalty * i
+	for j in range (0,len(m)+1):
+		score[0][j] = gap_penalty * j
+		
 	return M
+
+def match_score(alpha,beta):
+	if alpha == beta:
+		return match_award
+	elif alpha == "-" or beta == "-":
+		return gap_penalty
+	else:
+		return mismatch_penalty
+
+def needle(seq1,seq2):
+	score = init_matrix(seq1,seq2)
 
 def main():
 	human = init_file("human.fasta")
